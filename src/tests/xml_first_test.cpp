3a3af6f31579b7ee1099e380f762a1ec31312d39
unrealyozora <tommaso.ceron@gmail.com>
2025-03-07 19:34:32 +0100

    final bugfix

--- src/tests/xml_first_test.cpp
+++ src/tests/xml_first_test.cpp
@@ -16,17 +16,11 @@ void XmlTest::test() {
     catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
     }
-    if (newLib->is_empty()) {
-        std::cout << "La lista e' vuota";
-    }
-    else {
         auto x = newLib->getList();
 
         for (unsigned int i = 0; i < newLib->getSize(); ++i) {
             std::cout << x[i]->getTitle() << std::endl;
         }
-        
-    }
 
     try {
         newLib->toXml(write_path);
